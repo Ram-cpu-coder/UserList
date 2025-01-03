004c411e098f915df7a73a11a30daec868c05040
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2025-01-04 09:38:17 +1100

    structuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import InputSection from "./InputSection";
+import OutputTable from "./OutputTable";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <div className="flex flex-col justify-center items-center gap-5">
+      <div className="flex flex-col justify-center items-center h-[auto] w-[85vw] mt-5">
+        <h1 className="text-5xl font-bold">User List</h1>
+        <hr className="w-full m-2" />
+        <InputSection />
+      </div>
+      <OutputTable />
+    </div>
+  );
 }
 
 export default App;
--- src/InputSection.jsx
+++ src/InputSection.jsx
@@ -1,18 +1,26 @@
 "use client";
 import React from "react";
 
-import { Label, Select } from "flowbite-react";
+import { Label, Select, TextInput, Button } from "flowbite-react";
 
 const InputSection = () => {
   return (
-    <div className="max-w-md">
+    <div className="max-w-md w-full">
       <div className="mb-2 block">
-        <Label htmlFor="countries" value="Select your country" />
+        <Label htmlFor="gender" value="Gender" />
       </div>
-      <Select id="countries" required>
+      <Select id="gender" required>
         <option>Male</option>
         <option>Female</option>
+        <option>Prefer Not To Say</option>
       </Select>
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor="name" value="Name" />
+        </div>
+        <TextInput id="name" type="text" sizing="md" />
+      </div>
+      <Button className="my-2 w-full">Add User</Button>
     </div>
   );
 };
